ae171a580a48da54db84fbf4105ea9405414244c
gamadv <lealdev.py@gmail.com>
2023-03-13 10:44:40 -0300

    feat: Add some useful hooks

---
+++ react/src/hooks/useClickAway.ts
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+
+export function useClickAway(ref: any, handler: any) {
+  useEffect(() => {
+    const listener = (event: { target: any }) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    };
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+}
