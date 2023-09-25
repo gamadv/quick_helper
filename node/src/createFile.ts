9a0b93009ff85124657959325a0b8845c209802d
gamadv <lealdev.py@gmail.com>
2023-09-25 16:50:32 -0300

    Node: feat: add new props to Generate File function

--- node/src/createFile.ts
+++ node/src/createFile.ts
@@ -1,13 +1,19 @@
 import fs from 'fs'
+import path from 'path'
 
-export function generateConstantsFile(values: unknown, filename = 'myfile') {
-  const parseFileName = `${filename}.ts`
+export function generateConstantsFile(
+  values: unknown,
+  filename = 'unnamed_file',
+  filePath = 'src',
+) {
+  const parseFileName = `${filename}.gen.ts`
 
   const parseValues = `
   export const valueConstantCreated = ${JSON.stringify(values)};
   `
+  const fullFilePath = path.join(filePath, parseFileName)
 
-  fs.writeFile(`src/${parseFileName}`, parseValues, (err) => {
+  fs.writeFile(fullFilePath, parseValues, (err) => {
     if (err) {
       console.error(`Error writing ${filename}:`, err)
     } else {
