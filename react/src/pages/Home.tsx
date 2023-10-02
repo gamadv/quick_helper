ec23b446b3e1ba087d896cf538ed028fcbbd9577
gamadv <lealdev.py@gmail.com>
2023-10-02 09:18:36 -0300

    feat: Create a Mock Service with JSON Server

--- react/src/pages/Home.tsx
+++ react/src/pages/Home.tsx
@@ -2,8 +2,20 @@ import { PolyButton } from "../components/PolyButton";
 import { Testing } from "../components/Testing";
 import InputStyled from "../components/UI/InputStyled";
 import { CSVReader } from "../components/CSVReader";
+import { useEffect, useState } from "react";
+import { GetPosts } from "../services/api";
 
 export function Home() {
+
+  async function fetchData(){
+    const myresponse = await GetPosts()
+
+    console.log('🚀 ~ file: Home.tsx:13 ~ myresponse:', myresponse)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
   return (
     <main className="flex flex-col max-w-[1200px] m-auto py-6 bg-yellow-400">
       <section className="flex items-center max-w-md justify-between m-auto gap-4">
