dd80c82acba5535ac6f255e27e5342d7feb5d243
gamadv <lealdev.py@gmail.com>
2023-10-23 17:36:31 -0300

    chore: Add new features to test file

--- react/src/pages/Home.tsx
+++ react/src/pages/Home.tsx
@@ -1,13 +1,13 @@
-import { PolyButton } from "../components/PolyButton";
-import { Testing } from "../components/Testing";
-import InputStyled from "../components/UI/InputStyled";
-import { CSVReader } from "../components/CSVReader";
-import { useEffect, useState } from "react";
-import { GetPosts } from "../services/api";
+import { PolyButton } from '../components/PolyButton'
+import { Testing } from '../components/Testing'
+import InputStyled from '../components/UI/InputStyled'
+import { CSVReader } from '../components/CSVReader'
+import { useEffect, useState } from 'react'
+import { GetPosts } from '../services/api'
+import { Input } from '@chakra-ui/react'
 
 export function Home() {
-
-  async function fetchData(){
+  async function fetchData() {
     const myresponse = await GetPosts()
 
     console.log('🚀 ~ file: Home.tsx:13 ~ myresponse:', myresponse)
@@ -17,25 +17,28 @@ export function Home() {
     fetchData()
   }, [])
   return (
-    <main className="flex flex-col max-w-[1200px] m-auto py-6 bg-yellow-400">
-      <section className="flex items-center max-w-md justify-between m-auto gap-4">
+    <main className='flex flex-col max-w-[1600px] m-auto py-6 bg-yellow-400'>
+      <section className='flex items-center max-w-md justify-between m-auto gap-4'>
         <PolyButton>Default</PolyButton>
         <PolyButton asChild>
           <div>as DIV</div>
         </PolyButton>
         <PolyButton asChild>
-          <a href="#">as Anchor</a>
+          <a href='#'>as Anchor</a>
         </PolyButton>
       </section>
-      <section className="flex items-center max-w-md justify-between m-auto gap-4">
+      <section className='flex items-center max-w-md justify-between m-auto gap-4'>
         <InputStyled />
       </section>
-      <section className="flex items-center max-w-md justify-between m-auto gap-4">
+      <section className='flex items-center max-w-md justify-between m-auto gap-4'>
         <Testing />
       </section>
-      <section className="flex items-center max-w-md justify-between m-auto gap-4">
+      <section className='flex items-center max-w-md justify-between m-auto gap-4'>
         <CSVReader />
       </section>
+      <section>
+        <Input placeholder='Basic usage' />
+      </section>
     </main>
-  );
+  )
 }
