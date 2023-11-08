57535d7df1b673525361bc59eafb3c512ac2872f
gamadv <lealdev.py@gmail.com>
2023-11-08 20:11:21 -0300

    change default tests

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,24 +1,3 @@
-import { startParseInvestiments } from './investmentsParser/parser'
-
-const teste = [
-  {
-    nome: 'videoIndex',
-    valor: 'https://www.youtube.com/watch?v=EzsI3zWA8pE',
-  },
-  {
-    nome: 'qtdAulas',
-    valor: '400',
-  },
-  {
-    nome: 'qtdSerie',
-    valor: '60',
-  },
-  {
-    nome: 'qtdProfessor',
-    valor: '40',
-  },
-]
-
-const toConsole = teste.filter((param) => param.nome !== 'videoIndex')
+const toConsole = 'console'
 
 console.log('🚀 ~ file: index.ts:531 ~ toConsole:', toConsole)
